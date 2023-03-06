f35d23a031c4f34f00c2bc091fc7591025533016
Jonathan Bravo <j.ivanbravo98@gmail.com>
2023-03-05 21:30:47 -0300

    responsive

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Funcion para el menu responsive
+
+function responsiveMenu(){
+    let x = document.getElementById('nav');
+    if(x.className===""){
+        x.className ="responsive"
+    } else{
+        x.className =""
+    }
+}
